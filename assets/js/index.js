18520f995e758a7cf06131b23ba5608b73c45afa
捕风的逍遥侯 <aohanhongzhi@qq.com>
2021-07-23 07:23:41 +0000

    ageDiv

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,6 @@ var age = date .getFullYear() -1995;
 
 var ageDiv=document.getElementById("age");
 
-planet.innerHTML= age;
+ageDiv.innerHTML= age;
 
 console.log("感谢您的浏览，期待能够加入贵公司！我的gitee地址：https://gitee.com/aohanhongzhi");
